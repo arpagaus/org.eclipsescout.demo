08eae2ab3550f135a805db2b63fe557b70131cd5
Remo Arpagaus <remo.arpagaus@bsi-software.com>
2015-08-17 16:28:12 +0200

    Added missing @Order annotations

--- minicrm/org.eclipsescout.demo.minicrm.client/src/org/eclipsescout/demo/minicrm/client/ui/desktop/outlines/StandardOutline.java
+++ minicrm/org.eclipsescout.demo.minicrm.client/src/org/eclipsescout/demo/minicrm/client/ui/desktop/outlines/StandardOutline.java
@@ -12,6 +12,7 @@ package org.eclipsescout.demo.minicrm.client.ui.desktop.outlines;
 
 import java.util.List;
 
+import org.eclipse.scout.commons.annotations.Order;
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
 import org.eclipse.scout.rt.extension.client.ui.desktop.outline.AbstractExtensibleOutline;
@@ -19,6 +20,7 @@ import org.eclipse.scout.rt.shared.TEXTS;
 import org.eclipsescout.demo.minicrm.client.ui.desktop.outlines.pages.CompanyTablePage;
 import org.eclipsescout.demo.minicrm.client.ui.desktop.outlines.pages.PersonTablePage;
 
+@Order(100)
 public class StandardOutline extends AbstractExtensibleOutline {
 
   @Override
--- org.eclipsescout.demo.minicrm.client.extension/src/org/eclipsescout/demo/minicrm/client/extension/ui/desktop/outlines/ExtensionOutline.java
+++ org.eclipsescout.demo.minicrm.client.extension/src/org/eclipsescout/demo/minicrm/client/extension/ui/desktop/outlines/ExtensionOutline.java
@@ -1,14 +1,17 @@
 /**
- * 
+ *
  */
 package org.eclipsescout.demo.minicrm.client.extension.ui.desktop.outlines;
 
+import org.eclipse.scout.commons.annotations.Order;
 import org.eclipse.scout.rt.extension.client.ui.desktop.outline.AbstractExtensibleOutline;
 import org.eclipse.scout.rt.shared.TEXTS;
 
 /**
  * @author rar
  */
+
+@Order(200)
 public class ExtensionOutline extends AbstractExtensibleOutline {
 
   @Override
